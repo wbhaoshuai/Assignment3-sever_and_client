329e2debf2e9d067cab2fd7f26c0ff731bcf0949
wbhaoshuai <1402078577@qq.com>
2025-05-03 20:56:30 +0800

    Accept a client connection and Establish a thread to handle the requests from the client

--- sever.py
+++ sever.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 def start_server():
     host = 'localhost'
@@ -14,4 +15,17 @@ def start_server():
     server_socket.bind((host, port))
     server_socket.listen(10)
 
-    print("Server is running and waiting for client connection...")
+    print("Server is running and ready to accept multiple clients...")
+
+    try: 
+        while True:
+            # Accept a client connection
+            client_socket, addr = server_socket.accept()
+            # Establish a thread to handle the requests from the client
+            client_thread = threading.Thread(target = handle_client, args = (client_sock, addr))
+            client_thread.start()
+    except KeyboardInterrupt:
+        print("Shutting down the server.")
+    finally:
+        server_socket.close()
+
