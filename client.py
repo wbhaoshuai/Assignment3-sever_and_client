959c25888ef34332f2370b3ec6e85c60bf6eea87
wbhaoshuai <1402078577@qq.com>
2025-05-03 22:40:11 +0800

    Created the client's socket and its framework for executing task code

--- client.py
+++ client.py
@@ -2,6 +2,19 @@ import socket
 import threading
 import time
 
+def client_task(name):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', 51234))
+
+        # something client need todo
+        
+    except Exception as e:
+        print(f"Error for {name}: {e}")
+    finally:
+        client_socket.close()
+
+
 def main():
     clients = []
     for i in range(10):
